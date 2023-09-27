c02690c79f20946a461342b0086a2f9093538948
d-kja <89911360+d-kja@users.noreply.github.com>
2023-09-27 03:51:19 -0300

    chore(challenge-03): setup basic configuration

---
+++ challenges/challenge-03/src/env/index.ts
@@ -0,0 +1,25 @@
+import 'dotenv/config'
+import { z } from 'zod'
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
+  PORT: z.number().default(4000),
+  SERVER_HOST: z.string().default('0.0.0.0'),
+
+  DATABASE_URL: z.string(),
+  REDIS_URL: z.string(),
+  REDIS_PASSWORD: z.string(),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (!_env.success) {
+  const errorMessage = _env.error.format()
+
+  console.error(errorMessage)
+  throw new Error('Invalid environment variables')
+}
+
+export const env = _env.data
---
+++ challenges/challenge-03/src/libs/redis.ts
@@ -0,0 +1,7 @@
+import { env } from '@/env'
+import { createClient } from 'redis'
+
+export const redis = createClient({
+  url: env.REDIS_URL,
+  password: env.REDIS_PASSWORD,
+})
